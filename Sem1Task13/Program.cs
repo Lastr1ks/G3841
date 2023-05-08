5b865b73a5a8a99f50353d871f528bd31e6bd397
Lastr1ks <serebrunskii99@gmail.com>
2023-05-09 00:19:43 +0300

    add solution task 10,13,15

---
+++ Sem1Task13/Program.cs
@@ -0,0 +1,26 @@
+﻿// №13 Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+// что третьей цифры нет. 
+
+//Проверка длинны числа и запись его в Length
+string orgNumber=Console.ReadLine();
+int Length = orgNumber.Length;
+//Проверка на 0 и преобразование string в int
+int number = int.Parse(orgNumber??"0");
+
+
+
+//Проверка на более 2х значность
+if (number >= 100)
+{
+    //Убираем числа после 3 цифры
+    int result = (int)(number / Math.Pow(10, Length-3));
+    //Убираем числа до 3 цифры
+    result = result % 10;
+    //Вывод в консоль
+    Console.WriteLine(result);
+}
+else
+{
+    //Вывод в консоль
+    Console.WriteLine("Третьей цифры нет");
+}
