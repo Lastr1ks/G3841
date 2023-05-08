613c54ef8c45928923ffbdeef47c2a53726e8bc3
Lastr1ks <serebrunskii99@gmail.com>
2023-05-08 22:24:14 +0300

    add solution for task 9,11,12,14,16

---
+++ Sem1Task11/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача №11
+// Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа. 
+
+//Вариант 1
+// System.Random numSintezator = new System.Random();
+
+// char[] digits = numSintezator.Next(100,1000).ToString().ToCharArray();
+// Console.WriteLine(digits);
+// int digit1 = (int)digits[0]-48;
+// int digit2 = (int)digits[1]-48;
+// int digit3 = (int)digits[2]-48;
+// Console.WriteLine(digit1+" "+ digit3);
+
+//Вариант 2
+System.Random numSintezator = new System.Random();
+
+int num = numSintezator.Next(100,1000);
+Console.WriteLine(num);
+int digit1 = num/100;
+int digit3 = num%10;
+Console.WriteLine(digit1+""+ digit3);
+//Console.WriteLine(digit1*10 + digit3);
---
+++ Sem1Task16/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача №16
+// Напишите программу, которая принимает на вход
+// два числа и проверяет, является ли одно число
+// квадратом другого. 
+
+
+double number1 = double.Parse(Console.ReadLine()??"0");
+double number2 = double.Parse(Console.ReadLine()??"0");
+
+double result1 = Math.Pow(number1, 2);
+double result2 = Math.Pow(number2, 2);
+
+if ((result1 == number2)||(result2 == number1))
+{
+    Console.WriteLine("Одно число является квадратом другого.");
+}
+else
+{
+    Console.WriteLine("Одно число НЕ является квадратом другого.");
+}
