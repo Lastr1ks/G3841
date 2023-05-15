3361da058efd141be0ee3d1084f056cc36fa0152
Lastr1ks <serebrunskii99@gmail.com>
2023-05-15 14:52:30 +0300

    g# Please enter the commit message for your changes. Lines starting

---
+++ Sem3Task20/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача №20
+// Напишите программу, которая принимает на вход
+// координаты двух точек и находит расстояние между
+// ними в 2D пространстве.
+
+//Запрашиваем координаты точек
+int x1 = ReadData("Введите координату x1");
+int x2 = ReadData("Введите координату x2");
+int y1 = ReadData("Введите координату y1");
+int y2 = ReadData("Введите координату y2");
+
+//Вычисляем длинну
+double Length = CulcLen(x1, x2, y1, y2);
+
+//Выводим результат в консоль
+PrintData("Расстояние между точками =", Length);
+
+
+
+
+
+//Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+//Метод выводит данные в консоль
+void PrintData(string msg, double Length)
+{
+    Console.WriteLine(msg + Length);
+}
+//Метод находит расстояние между точками
+double CulcLen(int x1, int x2, int y1, int y2)
+{
+    double res = 0;
+    res = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+    return res;
+}
+
---
+++ Sem3Task22/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача №22
+// Напишите программу, которая принимает на вход
+// число (N) и выдаёт таблицу квадратов чисел от 1 до
+// N.
+
+// Запрашиваем число и степень
+int num = ReadData("Введите число");
+int pow = ReadData("Введите степень");
+
+// Вызываем метод
+string res1 = LineBuilder(num, 1);
+string res2 = LineBuilder(num, pow);
+
+// Выводим результат
+PrintData(res1, res2);
+
+//Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+//Метод выводит данные в консоль
+void PrintData(string result1, string result2)
+{
+    Console.WriteLine(result1);
+    Console.WriteLine(result2);
+}
+//Метод выводит числа в нужной степени
+string LineBuilder(int number, int pow)
+{
+    string res = string.Empty;
+    for (int i = 1; i <= number; i++)
+    {
+         res+= Math.Pow(i, pow).ToString()+"\t";
+    }
+  
+    return res;
+}
+
